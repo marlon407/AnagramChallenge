c7256c5997e0c053cc6c1e7a262b1df3b8ea36da
Marlon Franca <marlondiego407@gmail.com>
2016-02-15 16:05:59 -0200

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160210221326) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "anagram_detectors", force: :cascade do |t|
     t.text     "first_text"
     t.string   "second_text"
